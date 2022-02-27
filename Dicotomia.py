c87860dd74a8a41ac456c40a7586b5db4752528e
acasasaez <acasasae@myuax.com>
2022-02-27 15:57:36 +0100

    precondición dicotomía

--- Dicotomia.py
+++ Dicotomia.py
@@ -2,6 +2,8 @@
 from re import T
 from tkinter import _EntryValidateCommand
 
+from pkg_resources import NullProvider
+
 
 Algoritmo dicotomia:
     #Indice de "t" en tabla o AUSENTE
@@ -9,3 +11,12 @@ entrada
     tabla[T-> COMPARABLE] #Objetivo de búsuqeda
     t: T  #lo que buscamos 
 Resultado: ENTERO #Número de celda que contiene "t" o AUSENTE.
+
+precondicion
+    #se han inicializado "t" Y "tabla"
+    t ≠ NULO 
+    tabla ≠ NULO 
+    #"tabla" ordenada en orden creciente 
+    #"t" está en el orden de los componentes de "tabla" 
+    tabla[indice_min(tabla)]<= t <= tabla[indice_max(tabla)]
+
