63e713f4b28ecc7c36f2a3e29a2cd96c44c5ee26
acasasaez <acasasae@myuax.com>
2022-02-27 15:43:32 +0100

    entrada dicotomia

---
+++ Dicotomia.py
@@ -0,0 +1,11 @@
+#Primera parte: Definimos la fundicón dicotomía
+from re import T
+from tkinter import _EntryValidateCommand
+
+
+Algoritmo dicotomia:
+    #Indice de "t" en tabla o AUSENTE
+entrada 
+    tabla[T-> COMPARABLE] #Objetivo de búsuqeda
+    t: T  #lo que buscamos 
+Resultado: ENTERO #Número de celda que contiene "t" o AUSENTE.
