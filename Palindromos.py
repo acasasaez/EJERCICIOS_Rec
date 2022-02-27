086e66866c014249220fe16e0f61bcc10933812e
acasasaez <acasasae@myuax.com>
2022-02-27 17:27:41 +0100

    Ejercicio Palindromos: Algoritmo de filtración:precondición

---
+++ Palindromos.py
@@ -0,0 +1,10 @@
+#Ejercicio Palíndromos 
+#Primera Parte: Filtración
+from pkg_resources import NullProvider
+
+
+Algoritmo alfanumerico (ca: CADENA): CADENA
+    #La copia de "ca" sin los caracteres no alfanuméricos.
+precondicion
+    ca ≠ NULO 
+    
